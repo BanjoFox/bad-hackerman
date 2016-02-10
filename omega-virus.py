eefa1f039d935a7242bb14bdb6f672db1ff24302
DrQuixote <pipnflinx@gmail.com>
2016-02-10 18:50:22 -0500

    Create omega-virus.py
    Starter skeleton... most of these probably aren't functions but hey... I'm feeling lazy.

---
+++ omega-virus.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python
+
+#
+# Insert docopt user help menu here?
+#
+
+
+#
+# End docopt
+#
+
+def sectors()
+	# Blue
+	# Green
+	# Red
+	# Yellow
+	
+def roomList()
+	# List of rooms
+	# Green (open rooms)
+	# Blue (requires blue key)
+	# Red (requires red key)
+	# Yellow (requires yellow key)
+
+def roomContents()
+	# Each room can have one of:
+	# 1. An ADV
+	# 2. An access card
+	# 3. A Probe (more on this in a little bit)
+	# 4. A hazard
+	# 5. The virus (presumably chilling out)
+	# 6. Nothing at all
+
+def items()
+	# Access keys (Blue, Red, Yellow)
+	# Decoder - yellow
+	# Disruptor - blue
+	# Negatron - red
+	# Probe
+	
+def players()
+	# Blue
+	# Green
+	# Red
+	# Yellow
+	
+def rng()
+	# Random number generator
+	# Values 0,1,2
+	
+def secretCode()
+	# Secret codes let players know where the virus is provided:
+	#	a) They enter a room where the virus is
+	#	b) They do not have all three weapons
+	#	c) Should we let probes find the virus?
+
