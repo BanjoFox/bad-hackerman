c31340f6b241c9568bdfe01120395bb41b187f18
DrQuixote <pipnflinx@gmail.com>
2016-02-17 14:58:31 -0500

    Added A LOT more functionality. Likely that none of it works though... but the IDEAS are there :D

--- omega-virus.py
+++ omega-virus.py
@@ -13,40 +13,98 @@
 import sys
 import random
 from random import randint
+import time
 
-def difficulty();
+
+def difficulty():
 	# Define the length of the game (these values are probably wrong :D Need to check rulebook.
 	# 0 - 30 Minutes
 	# 1 - 20 Minutes
 	# 2 - 15 Minutes
-	return minutes
+	# 3 - Custom -- Do we -REALLY- want to do this?
+
+	usrDiff = raw_input("Select a difficulty [0,1 or 2]: ")
 	
-def numPlayers();
+	if usrDiff == "0":
+		gameDiff = 30
+	
+	elif usrDiff == "1":
+		gameDiff = 20
+		
+	elif usrDiff == "2":
+		gameDiff = 15
+
+	elif usrDiff == "4":
+		gameDiff = raw_input("Fine... pick two numbers then: ")
+
+	else:
+		print("Don't like those options? NO GAME 4 U!")
+		quit()
+
+	return gameDiff
+	
+def numPlayers():
 	# In the orginal game, the Computer asks each player to enter their secret code at the beginning of the game.
 	# This then defines which colors are active. There may be a simpler way of doing this though...
-	return playerCount
 	
-def sectors()
+	
+	
+	
+	return playerCount
+
+def timer():
+	run = raw_input("Start? > ")
+	mins = gameDiff
+	# Only run if the user types in "start"
+	if run == "start":
+		# When 9 minutes are left one sector is shut down, and every 3 minutes after this another is shut down.
+		while mins != 9:
+			# Keep playing
+			# Sleep for a minute
+			time.sleep(60)
+		
+		sectorList = ['blue', 'green', 'red', 'yellow']
+		shutdown = random.choice(sectorList)
+		print(shutdown + "Sector Shutdown!")
+		
+		# Build new sector list
+		sectorList == sectorList - shutdown
+		
+		# Move all players to docking bay of random sector on the new list
+		teleport = random.choice(sectorList)
+
+		# Wash, rinse, repeat until: Virus wins (all sectors shutdown) OR Players win (kill virus)
+		# I KNOW there is a better way to loop this...
+		'''
+		while mins != 6:
+			# Keep playing
+			# Sleep for a minute
+			time.sleep(60)
+		
+		sectorList = ['blue', 'green', 'red', 'yellow']
+		shutdown = random.choice(sectorList)
+		print(shutdown + "Sector Shutdown!")
+		
+		# Build new sector list
+		sectorList == sectorList - shutdown
+		
+		# Move all players to docking bay of random sector on the new list
+		teleport = random.choice(sectorList
+		'''
+
+
+# Commenting this out because we -probably- don't need it.
+'''		
+def sectors():
 	# Docking Bays (Blue, Green, Red, Yellow)
 	# Blue Rooms
 	# Green Rooms
 	# Red Rooms
 	# Yellow Rooms
-	# When 9 minutes are left one sector is shut down, and every 3 minutes after this another is shut down.
-	sectorList = ['blue', 'green', 'red', 'yellow']
-	if minutes = 9 then
-		shutdown = random.choice(sectorList)
-		print(shutdown + "Sector Shutdown!")
-		
-		teleport = random.choice(sectorList)
-		# Remove the shutdown sector from the list, and
-		# Move all players to docking bay of random sector on the new list
-		
-	else 
-		#keep the timer going
+'''
 		
 	
-def roomList();
+def roomList():
 	# Docking Bays (Blue, Green, Red, Yellow)
 	# List of rooms
 	# Green (open rooms)
@@ -54,7 +112,7 @@ def roomList();
 	# Red (requires red key)
 	# Yellow (requires yellow key)
 
-def roomContents();
+def roomContents():
 	# Each room can have one of:
 	# 1. An ADV
 	# 2. An access card
@@ -63,7 +121,7 @@ def roomContents();
 	# 5. The virus (presumably chilling out)
 	# 6. Nothing at all
 
-def items();
+def items():
 	# Four of each, to be distributed across the map.
 	# Access keys (Blue, Red, Yellow)
 	# Decoder - yellow
@@ -71,9 +129,9 @@ def items();
 	# Negatron - red
 	# Probe
 	
-def playerBag();
+def playerBag():
 	# A place to define inventory. 
-	def bluePlayer();
+	def bluePlayer():
 		blueKey = boolean
 		redKey = boolean
 		yellowKey = boolean
@@ -83,7 +141,7 @@ def playerBag();
 		negatron = boolean
 	return blueInventory
 	
-	def greenPlayer();
+	def greenPlayer():
 		blueKey = boolean
 		redKey = boolean
 		yellowKey = boolean
@@ -93,7 +151,7 @@ def playerBag();
 		negatron = boolean
 	return blueInventory
 
-	def redPlayer();
+	def redPlayer():
 		blueKey = boolean
 		redKey = boolean
 		yellowKey = boolean
@@ -103,7 +161,7 @@ def playerBag();
 		negatron = boolean
 	return blueInventory
 
-	def yellowPlayer();
+	def yellowPlayer():
 		blueKey = boolean
 		redKey = boolean
 		yellowKey = boolean
@@ -113,25 +171,25 @@ def playerBag();
 		negatron = boolean
 	return blueInventory
 	
-def probes();
+def probes():
 	# Probes can move, explore and be attacked just like players. However they cannot attack themselves.
 	# Blue
 	# Green
 	# Red
 	# Yellow
 	
-def rng();
+def rng():
 	# Random number generator ranging from 0-2
 	rng = randint(0,2)
 	return rng
 	
-def secretCode();
+def secretCode():
 	# Secret codes let players know where the virus is provided:
 	#	a) They enter a room where the virus is
 	#	b) They do not have all three weapons
 	#	c) Should we let probes find the virus?
 	
-def userInput();
+def userInput():
 	# We will need a way for players to input numbers (0,1,2)
 	# This is for room numbers, secret codes, and combat
 	blueCode = raw_input("Blue player enter secret code: ")
@@ -139,13 +197,13 @@ def userInput();
 	redCode = raw_input("Red player enter secret code: ")
 	yellowCode = raw_input("Yellow player enter secret code: ")
 	
-def combat();
+def combat():
 	# Combat includes, players attacking one another (or their drones) as well as attaking the virus. 
 	# Defender picks a value (0,1 or 2), and the Attacker picks one as well.
 	# If both match, then the attacker wins. I.e. the Virus is killed, the player looses an item (at random),
 	# or the drone dies.
 	
-def phrasing();
+def phrasing():
 	# This is a placeholder for all of the phrases that are used throuout the game.
 	# --Virus--
 	# "You Human Scum!"
